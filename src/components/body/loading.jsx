a13d6559abcd483a6c87408aaf882ee6e49a982a
Pratik Thapa <pratikthapa@Dark-Beauty-16.local>
2022-11-12 15:18:39 -0700

    render successful and components ready

---
+++ src/components/body/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function loading() {
+  return (
+    <section className='loading-center'>
+      <div className='loading'></div>
+    </section>
+  );
+}
+
+export default loading;
