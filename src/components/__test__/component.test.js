56e84ccfd8fe7790a14d6faf1e0d577e2c94b5cd
Pratik Thapa <pratikthapa@Dark-Beauty-16.local>
2022-11-20 16:43:50 -0700

    unit testing dynamic component

--- src/components/__test__/component.test.js
+++ src/components/__test__/component.test.js
@@ -1,7 +1,16 @@
-import { render, screen, cleanup, fireEvent } from '@testing-library/react';
+import {
+  render,
+  screen,
+  cleanup,
+  fireEvent,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import NavigationBar from '../navigation/navbar';
 import Search from '../body/search';
 import AddressSection from '../body/addressSection';
+import userEvent from '@testing-library/user-event';
+import App from '../../App';
 
 describe('Static Component Testing', () => {
   test('should render navigaiton bar', () => {
@@ -67,7 +76,38 @@ describe('Dynamic Components Testing', () => {
     expect(displayedResult).toContainElement(businessAddress);
     expect(displayedResult).toContainElement(businessPhone);
   });
-  test('Error Message Appears', () => {
-    // TODO:
+
+  test('error Message does not appear initially without button click', () => {
+    render(<Search />);
+    const errorMessage = screen.queryByRole('alert', {
+      name: /error message/i,
+    });
+    expect(errorMessage).not.toBeInTheDocument();
+  });
+
+  test('error Message Appears', async () => {
+    render(<App />);
+
+    const searchButton = screen.getByRole('button', { name: /search button/i });
+    userEvent.click(searchButton);
+
+    const loadingSpinner = screen.queryByRole('alert', {
+      name: /loading content/i,
+    });
+
+    await waitForElementToBeRemoved(() => loadingSpinner);
+
+    const errorMessage = await screen.getByRole('alert', {
+      name: /error message/i,
+    });
+    expect(errorMessage).toBeInTheDocument();
+    expect(errorMessage).toBeVisible();
+  });
+
+  test('do not show api loading before search', () => {
+    const loadingSpinner = screen.queryByRole('alert', {
+      name: /loading content/i,
+    });
+    expect(loadingSpinner).not.toBeInTheDocument();
   });
 });
